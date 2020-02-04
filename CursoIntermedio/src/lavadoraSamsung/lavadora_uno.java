cf4eddf15810f49b5fbd46caca5f5ad911374291
adolfo <ahideroa@gmail.com>
2020-02-03 20:56:57 -0600

    commit desde lap2

---
+++ CursoIntermedio/src/lavadoraSamsung/lavadora_uno.java
@@ -0,0 +1,24 @@
+package lavadoraSamsung;
+
+import encapsulamiento.Llfunciones;
+import java.util.Scanner;
+
+public class lavadora_uno {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("La ropa es blanca o de color?");
+		System.out.println("1: Ropa Blanca / 2: Ropa de Color");
+		int tipoDeRopa = entrada.nextInt();
+		
+		System.out.println("Cuantos kilos de ropa?");
+		int kilos = entrada.nextInt();
+		
+		Llfunciones mensajero = new Llfunciones(kilos, tipoDeRopa);
+		mensajero.CicloFinalizado();
+
+	}
+
+}
