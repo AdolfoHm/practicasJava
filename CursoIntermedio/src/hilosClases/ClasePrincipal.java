c85f687d97a3a5414ef510857977b87900720a29
adolfo <ahideroa@gmail.com>
2020-02-16 20:15:21 -0600

    Practicas 22 y 23

---
+++ CursoIntermedio/src/hilosClases/ClasePrincipal.java
@@ -0,0 +1,16 @@
+package hilosClases;
+
+public class ClasePrincipal {
+	
+	public static void main(String args[]) {
+		
+		Proceso1 hilo1 = new Proceso1();
+		Thread hilo2 = new Thread(new Proceso2());
+		Proceso1 hilo3 = new Proceso1();
+		
+		hilo1.start();
+		hilo2.start();
+		hilo3.start();
+		
+	}
+}
