42075caf2e297dd85a960e7b1c721c44bd7b03d7
adolfo <ahideroa@gmail.com>
2020-02-23 19:24:25 -0600

    Practicas 26, 27 y 28

---
+++ CursoIntermedio/src/claseMath/Constantes.java
@@ -0,0 +1,11 @@
+package claseMath;
+
+public class Constantes {
+	public static void main(String args[]) {
+		
+		System.out.println("El numero e: " + Math.E);
+		System.out.println("El valor de pi: " + Math.PI);
+		
+	}
+
+}
---
+++ CursoIntermedio/src/constructor/Principal.java
@@ -0,0 +1,14 @@
+package constructor;
+
+public class Principal {
+	public static void main(String args[]) {
+		
+//		SinConstructor mensajero = new SinConstructor();
+//		mensajero.perdirNombre();
+//		mensajero.imprimir();
+		
+		ConConstructor mensajero = new ConConstructor();
+
+	}
+
+}
