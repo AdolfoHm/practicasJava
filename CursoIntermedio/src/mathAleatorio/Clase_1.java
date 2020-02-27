675186edb9e75c512287d550bd1fb7f53f78f2be
adolfo <ahideroa@gmail.com>
2020-02-26 23:31:12 -0600

    Practica 29

---
+++ CursoIntermedio/src/mathAleatorio/Clase_1.java
@@ -0,0 +1,14 @@
+package mathAleatorio;
+
+public class Clase_1 {
+
+	public static void main(String[] args) {
+		
+		int aleatorio = 0;
+		aleatorio = (int)(Math.random() * 99);
+		
+		System.out.println(aleatorio);
+
+	}
+
+}
---
+++ CursoIntermedio/src/mathAleatorio/Clase_2.java
@@ -0,0 +1,17 @@
+package mathAleatorio;
+
+import java.util.Random;
+
+public class Clase_2 {
+	public static void main(String args[]) {
+		
+		int aleatorio = 0;
+		
+		Random r = new Random();
+		aleatorio = (int) (r.nextDouble() * 100);
+		
+		System.out.println(aleatorio);
+		
+	}
+
+}
